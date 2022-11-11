bca1fb4718ed446e2130d6419075392d2d4c3331
ravik <ravik@DESKTOP-93PTIUJ>
2022-11-11 12:35:01 +0530

    push1 by manager

---
+++ src/test/java/master/p1.java
@@ -0,0 +1,10 @@
+package master;
+
+public class p1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("p1");
+	}
+
+}
---
+++ src/test/java/master/p2.java
@@ -0,0 +1,10 @@
+package master;
+
+public class p2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("p2");
+	}
+
+}
